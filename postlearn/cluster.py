e4063e6392c410ca512ee9c7125aa7d963fffce5
Tom Augspurger <tom.augspurger88@gmail.com>
2016-11-16 10:29:58 -0600

    ENH: cluster

---
+++ postlearn/cluster.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KDTree
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+
+from postlearn.utils import rediscretize_cmap
+
+
+def plot_decision_boundry(data, pipe, reducer=PCA):
+    fig, ax = plt.subplots(figsize=(16, 12))
+    reducer = reducer()
+
+    if isinstance(pipe, Pipeline):
+        prepipe = Pipeline(pipe.steps[:-1])
+        km = pipe.steps[-1][1]
+        data_ = prepipe.transform(data)
+    else:
+        prepipe = None
+        km = pipe
+        data_ = data
+
+    X_reduced = reducer.fit_transform(data_)
+    mu_reduced = reducer.transform(km.cluster_centers_)
+    n_clusters = len(np.unique(km.labels_))
+    tree = KDTree(mu_reduced)
+
+    cmap = rediscretize_cmap(n_clusters, 'Set1')
+    sc = ax.scatter(mu_reduced[:, 0], mu_reduced[:, 1],
+                    c=np.arange(km.n_clusters), cmap=cmap,
+                    s=300)
+    plt.colorbar(sc, ticks=np.arange(n_clusters))
+    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], c=km.labels_,
+               cmap=cmap, alpha=.95)
+
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+
+    xx, yy = np.meshgrid(np.linspace(xmin, xmax, 100),
+                         np.linspace(ymin, ymax, 100))
+    T = np.c_[xx.ravel(), yy.ravel()]
+    _, group = tree.query(T)
+
+    Z = group.ravel().reshape(xx.shape)
+    ax.pcolormesh(xx, yy, Z, alpha=.25, cmap=cmap)
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+
+    for label, xy in enumerate(mu_reduced[:, :2]):
+        ax.annotate(label, xy, fontsize=28, fontweight="bold")
+    return ax
--- postlearn/utils.py
+++ postlearn/utils.py
@@ -18,3 +18,21 @@ def model_from_pipeline(pipe):
     else:
         return pipe
 
+
+def discrete_cmap(N, base_cmap=None):
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    return base.from_list(cmap_name, color_list, N)
+
+
+def rediscretize_cmap(N, base_cmap=None):
+    base = plt.cm.get_cmap(base_cmap)
+    step = base.N // N
+
+    if hasattr(base, 'colors'):
+        colors = base.colors[0:base.N, step]
+    else:
+        colors = base(np.arange(0, base.N, step))
+    name = base.name + str(N)
+    return mp.colors.ListedColormap(colors, name, N)
